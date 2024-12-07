4a9ae29c2e062112de5459652e0142676bad7f60
bushraabir <muzahidulislamabir123@gmail.com>
2024-12-07 18:59:10 +0600

    scene setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Suspense, useState, useEffect } from 'react';
 import Island from '../models/Island';
 import Loader from '../components/Loader';
 import { Sky } from '@react-three/drei';
+import backgroundImage from '../assets/11.jpg';
 
 const Home = () => {
   const [islandScale, setIslandScale] = useState([0.9, 0.9, 0.9]);
@@ -25,15 +26,23 @@ const Home = () => {
   return (
     <section className="w-full h-screen relative">
       <Suspense fallback={<Loader />}>
-        <Canvas
-          className="w-full h-screen bg-transparent"
-          camera={{ near: 0.1, far: 1000, position: [0, 0, 5] }}
-        >
+      <Canvas
+          className="w-full h-screen"
+          style={{
+            backgroundImage:
+            `linear-gradient(to top, rgba(249, 115, 22, 0.15), rgba(249, 115, 22, 0.15)), ` +
+            `url(${backgroundImage})`,  // Background image with gradient overlay
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            filter: 'brightness(1)',  // Lighten the background for a glowing effect
+          }}
+            camera={{ near: 0.1, far: 1000, position: [0, 0, 5] }}
+>
           {/* Lights */}
           <pointLight intensity={0.8} />
-          <ambientLight intensity={0.02} />
-          <spotLight intensity={0.5} />
-          <hemisphereLight intensity={0.5} skyColor="#b1e1ff" groundColor="#000000" />
+          <ambientLight intensity={.02} />
+          <spotLight intensity={1.5} />
+          <hemisphereLight intensity={0.5} skyColor=" rgba(249, 115, 22, 0.15)" groundColor="#000000" />
           <directionalLight position={[1, 1, 1]} intensity={0.05} />
 
           {/* Sky Component */}
