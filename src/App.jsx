cbf884797b0faff2209d250a6b3398e00f1ea4e3
bushraabir <muzahidulislamabir123@gmail.com>
2025-01-09 23:44:45 +0600

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import { About_us, Home, Features,  Contact } from "./pages"
+import Loader from "./components/Loader"
+
 
 const App = () => {
   return (
     <main className="bg-slate-300/20">
-        <Router>
+      <Router>
+        {loading ? (
+          <Loader />
+        ) : (
+          <>
             <Navbar />
             <Routes>
-              <Route path='/' element={<Home />} />
+              <Route path='/empowereducation/' element={<Home />} />
               <Route path='/about_us' element={<About_us />} />
               <Route path='/features' element={<Features />} />
               <Route path='/contact' element={<Contact />} />
             </Routes>
-           
-        </Router>
+          </>
+        )}
+      </Router>
     </main>
   )
 }
